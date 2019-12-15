a76263aed2bb4d3a896135779d907b51f64c5a2d
MlLimo <antoniorgc@hotmail.es>
2019-12-15 16:10:27 +0000

    Casi finalizada pagina portada

--- javascipt/footer.js
+++ javascipt/footer.js
@@ -2,3 +2,9 @@ function goTo(url)
 {
   location.href = url;
 }
+
+var include = function(beReplacedId){
+  jQuery.get('../views/templates/footer.html',function(data){
+      jQuery(beReplacedId).replaceWith(data);
+  
+});}
