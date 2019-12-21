41196ccb37c05bf3772892a60dd7916dd801c2d2
MlLimo <antoniorgc@hotmail.es>
2019-12-21 19:39:22 +0000

    Insertando Firebase a contacto

--- javascipt/contacto.js
+++ javascipt/contacto.js
@@ -1,6 +1,20 @@
 
 (function ($) {
 
+  var firebaseConfig = {
+    apiKey: "AIzaSyAlAobkJ2AHEA9mz0QhLcOdMgfXTnyj6qQ",
+    authDomain: "sipc-42387.firebaseapp.com",
+    databaseURL: "https://sipc-42387.firebaseio.com",
+    projectId: "sipc-42387",
+    storageBucket: "sipc-42387.appspot.com",
+    messagingSenderId: "671437049734",
+    appId: "1:671437049734:web:74403a0c47941d54e02607",
+    measurementId: "G-S30029EKQ4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  console.log(firebase);
+
   "use strict";
   $('.validate-input .input100').each(function(){
     $(this).on('blur', function(){
