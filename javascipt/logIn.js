a32b3739f857f8c7563f228fb248e32f6355200a
MlLimo <antoniorgc@hotmail.es>
2019-12-26 16:45:24 +0000

    firebase

--- javascipt/logIn.js
+++ javascipt/logIn.js
@@ -1,28 +1,3 @@
-
-//===================================
-//            FIREBASE
-//===================================
-  var firebaseConfig = {
-    apiKey: "AIzaSyAlAobkJ2AHEA9mz0QhLcOdMgfXTnyj6qQ",
-    authDomain: "sipc-42387.firebaseapp.com",
-    databaseURL: "https://sipc-42387.firebaseio.com",
-    projectId: "sipc-42387",
-    storageBucket: "sipc-42387.appspot.com",
-    messagingSenderId: "671437049734",
-    appId: "1:671437049734:web:74403a0c47941d54e02607",
-    measurementId: "G-S30029EKQ4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  console.log(firebase);
-
-  var database = firebase.database();
-
-
-//===================================
-//            !FIREBASE
-//===================================
-
 firebase.auth().onAuthStateChanged(function(user) {
   if (user) {
     // User is signed in.
