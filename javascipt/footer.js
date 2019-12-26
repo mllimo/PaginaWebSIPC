dff06b2e7091a63e3dfb7d69e7e1e2c7e63b7a79
MlLimo <antoniorgc@hotmail.es>
2019-12-26 16:38:08 +0000

    Nuevas funcionalidades js

---
+++ javascipt/footer.js
@@ -0,0 +1,10 @@
+function goTo(url)
+{
+  location.href = url;
+}
+
+var include = function(beReplacedId){
+  jQuery.get('../views/templates/footer.html',function(data){
+      jQuery(beReplacedId).replaceWith(data);
+  
+});}
--- javascipt/logIn.js
+++ javascipt/logIn.js
@@ -23,9 +23,21 @@
 //            !FIREBASE
 //===================================
 
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+  } else {
+    // No user is signed in.
+  }
+});
 
 function login (){
   var userEmail  = document.getElementById('email').value;
   var userPass   = document.getElementById('password').value;
-  console.log(userEmail + userPass);
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    window.alert("Error : " + errorMessage);
+  });
 }
