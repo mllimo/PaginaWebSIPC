297683b513812e491f5fd361146889743c046f44
MlLimo <antoniorgc@hotmail.es>
2019-12-29 11:45:15 +0000

    Colision contacto

--- javascript/footer.js
+++ javascript/footer.js
@@ -5,12 +5,6 @@ function goTo(url)
 
 var include = function(beReplacedId){
   jQuery.get('../views/templates/footer.html',function(data){
-<<<<<<< HEAD
       jQuery(beReplacedId).replaceWith(data);  
 });}
 
-=======
-      jQuery(beReplacedId).replaceWith(data);
-  
-});}
->>>>>>> Contacto
